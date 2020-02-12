c080ba5e573540110882ef489c9728d9232127a8
Unknown <ranjurave@gmail.com>
2020-02-12 16:35:55 +1300

    Lesson 7 & 8
    8 not complete

---
+++ CPPLesson07/Source/CPPLesson02/FPSExtractionZone.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "FPSExtractionZone.h"
+#include "CPPLesson02Character.h"
+#include "CPPLesson02GameMode.h"
+#include "Kismet/GameplayStatics.h"
+#include "Components/BoxComponent.h"	
+
+// Sets default values
+AFPSExtractionZone::AFPSExtractionZone()
+{
+
+	OverlapComp = CreateDefaultSubobject<UBoxComponent>(TEXT("OverlapComp"));
+	OverlapComp->SetCollisionEnabled(ECollisionEnabled::QueryOnly);
+	OverlapComp->SetCollisionResponseToAllChannels(ECR_Ignore);
+	OverlapComp->SetCollisionResponseToChannel(ECC_Pawn, ECR_Overlap);
+	OverlapComp->SetBoxExtent(FVector(200.0f));
+	RootComponent = OverlapComp;
+
+	OverlapComp->OnComponentBeginOverlap.AddDynamic(this, &AFPSExtractionZone::HandleOverlap);
+
+	OverlapComp->SetHiddenInGame(false);
+}
+
+// Called when the game starts or when spawned
+void AFPSExtractionZone::BeginPlay()
+{
+	Super::BeginPlay();
+
+}
+
+void AFPSExtractionZone::HandleOverlap(UPrimitiveComponent * OverlappedComponent, AActor * OtherActor, UPrimitiveComponent * OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult & SweepResult)
+{
+
+	ACPPLesson02Character* MyPawn = Cast<ACPPLesson02Character>(OtherActor);
+
+	if (MyPawn == nullptr)
+	{
+		return;
+	}
+	
+	if (MyPawn->bIsCarryingObjective)
+	{
+		ACPPLesson02GameMode* GM = Cast<ACPPLesson02GameMode>(GetWorld()->GetAuthGameMode());
+		if (GM)
+		{
+			GM->CompleteMission(MyPawn);
+		}
+	}
+	else
+	{
+		UGameplayStatics::PlaySound2D(this, ObjectiveMissingSound, 0.2f, 0.2f);
+	}
+	UE_LOG(LogTemp, Warning, TEXT("Overlapped with extraction zone"));
+}
+
---
+++ CPPLesson08/Source/CPPLesson02/CPPLesson02GameMode.cpp
@@ -0,0 +1,57 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "CPPLesson02GameMode.h"
+#include "CPPLesson02HUD.h"
+#include "CPPLesson02Character.h"
+#include "Kismet/GameplayStatics.h"
+#include "UObject/ConstructorHelpers.h"
+
+ACPPLesson02GameMode::ACPPLesson02GameMode()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPersonCPP/Blueprints/FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+	// use our custom HUD class
+	HUDClass = ACPPLesson02HUD::StaticClass();
+}
+
+void ACPPLesson02GameMode::CompleteMission(APawn* InstigatorPawn)
+{
+	if (InstigatorPawn)
+	{
+		InstigatorPawn->DisableInput(nullptr);
+		AActor* NewViewTarget;
+		TArray<AActor*> ReturnedActors;
+		UGameplayStatics::GetAllActorsOfClass(this, SpectatingCameraClass, ReturnedActors);
+
+		NewViewTarget = ReturnedActors[0];
+		APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
+
+		PC->SetViewTargetWithBlend(NewViewTarget, 5.0f, EViewTargetBlendFunction::VTBlend_Cubic);
+	}
+
+	OnMissionCompleted(InstigatorPawn);
+}
+
+	// Following is the best practice of checking each pointers 
+	//------------------------------------------------------------
+	//	if (SpectatingCameraClass)
+	//	{
+	//		TArray<AActor*> ReturnedActors;
+	//		UGameplayStatics::GetAllActorsOfClass(this, SpectatingCameraClass, ReturnedActors);
+	//		if (ReturnedActors.Num() > 0)
+	//		{
+	//			AActor* NewViewTarget = ReturnedActors[0];
+	//			APlayerController* PC = Cast<APlayerController>(InstigatorPawn->GetController());
+	//			if (PC)
+	//			{
+	//				PC->SetViewTargetWithBlend(NewViewTarget, 5.0f, EViewTargetBlendFunction::VTBlend_Cubic);
+	//			}
+	//		}
+	//	}
+	//	else
+	//	{
+	//		UE_LOG(LogTemp, Warning, TEXT("Not found?"))
+	//	}
+	//}
